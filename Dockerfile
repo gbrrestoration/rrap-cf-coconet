81410c6b33a9e5dba3fb2b3287538e73edf12524
Petridis, Ross (NCMI, Hobart) <Ross.Petridis@csiro.au>
2026-04-14 15:31:37 +1000

    Refactor Dockerfile and entrypoint for improved CoCoNet model setup that enables runtime config from invoking environment ; update docker-compose for environment variables and volume paths.

--- Dockerfile
+++ Dockerfile
@@ -16,25 +16,10 @@ RUN mkdir -p "${NETLOGO_HOME}"
 RUN wget -O - "https://downloads.netlogo.org/${NETLOGO_VERSION}/NetLogo-${NETLOGO_VERSION}-64.tgz" | tar -xz -C "${NETLOGO_HOME}" --strip-components=1
 LABEL org.netlogo.downloads.version=${NETLOGO_VERSION}
 
-# Install the CoCoNet data files and model code
-ENV COCONET_HOME="/opt/CoCoNet"
-ENV COCONET_MODEL_DIR="CoCoNet-model"
-WORKDIR "${COCONET_HOME}"
-# COPY ./CoCoNet-model "${COCONET_HOME}/"
-COPY --chmod=755 ./entrypoint.sh "${COCONET_HOME}/"
+COPY --chmod=755 ./entrypoint.sh "/entrypoint.sh"
 
-COPY --chmod=755 ./generate_experiment.sh "${COCONET_HOME}/"
-
-# Define environment variables that control how the CoCoNet model and 
-# and NetLogo platform will behave at runtime
-ENV COCONET_MODEL="${COCONET_HOME}/${COCONET_MODEL_DIR}/CoCoNet V3.0.nlogo" \
-    COCONET_OUT_DIR="${COCONET_HOME}/${COCONET_MODEL_DIR}/outputs" \
-    COCONET_PREFS_DIR="${COCONET_HOME}/${COCONET_MODEL_DIR}/.prefs" \
-    JAVA_TOOL_OPTIONS="" \
-    LANG="C.UTF-8" \
-    LC_ALL="C.UTF-8" \
-    MAX_RAM="8G"
+COPY --chmod=755 ./generate_experiment.sh "/generate_experiment.sh"
 
 # Configure the default entrypoint to launch the CoCoNet model
-ENTRYPOINT [ "./entrypoint.sh" ]
+ENTRYPOINT [ "/entrypoint.sh" ]
 CMD []
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -e
 
-# error for not providing OUTPUTS_DIR or COCONET_MODEL_DIR environment variables
-if [ -z "$OUTPUTS_DIR" ]; then
-    echo "!! ERROR !!: OUTPUTS_DIR environment variable is not set. Please set it to the name of the output directory (relative to COCONET_HOME: ${COCONET_HOME}) where CoCoNet will write its outputs. E.g., 'outputs' if it is at ${COCONET_HOME}/outputs"; exit 1
+if [ -z "$COCONET_HOME" ]; then
+    echo "!! ERROR !!: COCONET_HOME environment variable is not set. Please set it. This is where the model dir (CoCoNet-model) and outputs etc will be inside of."; exit 1
 fi
 
+export COCONET_MODEL_DIR="CoCoNet-model"
 
-if [ ! -d $COCONET_HOME/$COCONET_MODEL_DIR/$OUTPUTS_DIR ]; then
-    echo "!! ERROR !!: Output directory mount is required but not found at: COCONET_HOME/COCONET_MODEL_DIR/OUTPUTS_DIR = $COCONET_HOME/$COCONET_MODEL_DIR/$OUTPUTS_DIR. Program will likely FAIL! Customise this path with the OUTPUTS_DIR environment variable."; exit 1
+if [ ! -f $COCONET_HOME/$COCONET_MODEL_DIR/"CoCoNet V3.0.nlogo"  ]; then
+    echo "!! ERROR !!: CoCoNet model not found at:  ${COCONET_HOME}/${COCONET_MODEL_DIR}/CoCoNet V3.0.nlogo. Program will cannot run! Ensure the model is mounted inside COCONET_HOME = ${COCONET_HOME} with name CoCoNet-model"; exit 1
 fi
 
-if [ ! -d $COCONET_HOME/$COCONET_MODEL_DIR  ]; then
-    echo "!! ERROR !!: CoCoNet model mount is required but not found at: COCONET_HOME/COCONET_MODEL_DIR = $COCONET_HOME/$COCONET_MODEL_DIR. Program will likely FAIL! Customise this path with the COCONET_MODEL_DIR environment variable."; exit 1
-fi
+export COCONET_OUT_DIR="${COCONET_HOME}/${COCONET_MODEL_DIR}/outputs"
+export COCONET_MODEL="${COCONET_HOME}/${COCONET_MODEL_DIR}/CoCoNet V3.0.nlogo"
+export COCONET_PREFS_DIR="${COCONET_HOME}/${COCONET_MODEL_DIR}/.prefs"
+export JAVA_TOOL_OPTIONS=""
+export LANG="C.UTF-8"
+export LC_ALL="C.UTF-8"
+export MAX_RAM="8G"
 
 export JAVA_TOOL_OPTIONS="-Djava.awt.headless=true \
 -Xmx${MAX_RAM} \
@@ -22,17 +26,16 @@ export JAVA_TOOL_OPTIONS="-Djava.awt.headless=true \
 -Dfile.encoding=UTF-8 \
 ${JAVA_TOOL_OPTIONS:-}"
 
-# RUN mkdir -p "${COCONET_OUT_DIR}" "${COCONET_PREFS_DIR}" && \
-#     chmod 0777 "${COCONET_OUT_DIR}" && \
-#     chmod 0777 "${COCONET_PREFS_DIR}" && \
-#     chmod 0666 ./*.nlogo && \
-#     chmod 0666 ./*.csv && \
-#     chmod 0755 "entrypoint.sh" && \
-#     chmod 0755 "generate_experiment.sh"
-chmod -R ugo+rwX "${COCONET_HOME}"/*
+if [ ! -d $COCONET_OUT_DIR ]; then
+    echo "!! ERROR !!: Output directory mount is required but not found at: ${COCONET_OUT_DIR}."; exit 1
+fi
+
+
+
+chmod -R ugo+rwX "${COCONET_HOME}"
 
 # Generate experiment XML and parameter CSV from environment variables
-./generate_experiment.sh parameters
+/generate_experiment.sh parameters
 SETUP_FILE="parameters/generated_experiment.xml"
 
 echo "Setup file: ${SETUP_FILE}"
